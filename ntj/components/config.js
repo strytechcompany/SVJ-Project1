cddfc92b1f27a7121a3839bd327af78349a30014
strytechcompany <strytechcompany@gmail.com>
2026-02-23 02:16:22 +0530

    Update base_url to production API endpoint

--- ntj/components/config.js
+++ ntj/components/config.js
@@ -1,3 +1,5 @@
 // config.js
-export const base_url = 'http://192.168.29.73:3000/api';
-                            
+// export const base_url = 'http://localhost:3000/api';
+
+
+export const base_url = 'https://backend-ntj.netlify.app/api';
