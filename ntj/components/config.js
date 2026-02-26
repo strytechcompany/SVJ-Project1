1fe3a644cf7cfa9081d04a0dff9db635cb355dd2
strytechcompany <strytechcompany@gmail.com>
2026-02-26 10:23:10 +0530

    Update config.js

--- ntj/components/config.js
+++ ntj/components/config.js
@@ -2,4 +2,6 @@
 // export const base_url = 'http://localhost:3000/api';
 
 
-export const base_url = 'https://ntj-backend.onrender.com/api';
+// export const base_url = 'https://ntj-backend.onrender.com/api';
+
+export const base_url = 'https://ntj-62zb.onrender.com/api';
