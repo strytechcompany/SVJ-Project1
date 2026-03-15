2424e4ebc1edcd8224f82721104d29485db3d51e
SANJAY KISHORE <125728509+SANORE2003@users.noreply.github.com>
2026-03-15 13:37:58 +0530

    Delete ntj directory

--- ntj/components/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-// export const base_url = 'http://192.168.29.73:3000/api';
-
-
-// export const base_url = 'https://ntj-backend.onrender.com/api';
-
-export const base_url = 'https://ntj-62zb.onrender.com/api';
-
-// export const base_url = 'https://localhost:3000/api';
