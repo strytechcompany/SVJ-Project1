9bf4d459ad6632a1488c834155e64bc17a02ea5f
strytechcompany <strytechcompany@gmail.com>
2026-03-22 00:33:00 +0530

    Update CustomerB2C.js

--- Backend-ntj/routes/CustomerB2C.js
+++ Backend-ntj/routes/CustomerB2C.js
@@ -5,6 +5,7 @@ const {
     getCustomersB2C,
     getCustomerB2CById,
     updateCustomerB2C,
+    patchB2CBalances,
     deleteCustomerB2C
 } = require('../controllers/customerB2CController');
 
@@ -12,6 +13,7 @@ router.post('/', createCustomerB2C);
 router.get('/', getCustomersB2C);
 router.get('/:id', getCustomerB2CById);
 router.put('/:id', updateCustomerB2C);
+router.patch('/:id/balances', patchB2CBalances);
 router.delete('/:id', deleteCustomerB2C);
 
 module.exports = router;
