0bf7632bb1dddf7e306783128681bc901e18a8b2
strytechcompany <strytechcompany@gmail.com>
2026-03-25 23:28:55 +0530

    Update GstCustomer.js

--- Backend-ntj/models/GstCustomer.js
+++ Backend-ntj/models/GstCustomer.js
@@ -7,6 +7,27 @@ const gstCustomerSchema = new mongoose.Schema(
     address: { type: String, required: true, trim: true },
     gstin: { type: String, default: "", trim: true },
     date: { type: String, default: "" },
+    invoiceNo: { type: String, default: "", trim: true },
+    totalInvoiceValue: { type: String, default: "", trim: true },
+    billRows: [
+      {
+        sno: { type: String, default: "", trim: true },
+        particular: { type: String, default: "", trim: true },
+        hsnCode: { type: String, default: "", trim: true },
+        weight: { type: String, default: "", trim: true },
+        rate: { type: String, default: "", trim: true },
+        taxableValue: { type: String, default: "", trim: true },
+        cgst: { type: String, default: "", trim: true },
+        sgst: { type: String, default: "", trim: true },
+        total: { type: String, default: "", trim: true },
+      },
+    ],
+    bankDetails: {
+      accountName: { type: String, default: "", trim: true },
+      accountNo: { type: String, default: "", trim: true },
+      ifsc: { type: String, default: "", trim: true },
+      branch: { type: String, default: "", trim: true },
+    },
   },
   { timestamps: true }
 );
